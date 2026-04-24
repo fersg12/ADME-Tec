a1cc91a78f24c1b100f380c49b265555ac53376a
FerSG <fer.saldivarg@tec.mx>
2026-04-24 16:59:01 -0600

    update

--- ui/main.py
+++ ui/main.py
@@ -1,9 +1,8 @@
 import streamlit as st
-ICON_RED = "images/icon_red.png"
 
 pages = [
     st.Page("ADME-Tec_app.py", title="ADME-TEC"),
-    st.Page("1_Backgroung.py", title="Background"),
+    st.Page("1_Background.py", title="Background"),
     st.Page("2_Contact.py", title="Contact"),
 ]
 
